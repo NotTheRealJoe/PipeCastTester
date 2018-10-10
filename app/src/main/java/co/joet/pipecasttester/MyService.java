d8b53129e24f65828c86cb0bce816c6b81b8211d
NotTheRealJoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-10-10 11:18:42 -0500

    Integrate chromecast discovery to service file

--- app/src/main/java/co/joet/pipecasttester/MyService.java
+++ app/src/main/java/co/joet/pipecasttester/MyService.java
@@ -6,7 +6,16 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
+import org.schabi.newpipe.cast.Device;
+import org.schabi.newpipe.cast.protocols.chromecast.ChromecastDiscoverer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class MyService extends Service {
+    private ChromecastDiscoverer chromecastDiscoverer;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -15,13 +24,27 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Service start", Toast.LENGTH_LONG).show();
+        try {
+            this.chromecastDiscoverer = new ChromecastDiscoverer();
+            Toast.makeText(this, "Discovery Started", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Starting discovery failed!", Toast.LENGTH_LONG).show();
+        }
         return START_STICKY;
+    }
+
+    public List<Device> getChromecasts() throws InterruptedException, ExecutionException, IOException {
+        return chromecastDiscoverer.discoverDevices();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        try {
+            chromecastDiscoverer.finalize();
+        } catch (IOException e) {
+            Toast.makeText(this, "Failed to stop discovery", Toast.LENGTH_LONG).show();
+        }
         Toast.makeText(this, "Service destroyed", Toast.LENGTH_LONG).show();
     }
 }
