5a6e520ea3bd37c753831239404820ba30c697c3
NotTheRealJoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-10-10 09:34:07 -0500

    Add some sample code to get a working service

--- app/src/main/java/co/joet/pipecasttester/MainActivity.java
+++ app/src/main/java/co/joet/pipecasttester/MainActivity.java
@@ -1,7 +1,9 @@
 package co.joet.pipecasttester;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.startService(findViewById(android.R.id.content));
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
     }
 }
---
+++ app/src/main/java/co/joet/pipecasttester/MyService.java
@@ -0,0 +1,27 @@
+package co.joet.pipecasttester;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service start", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service destroyed", Toast.LENGTH_LONG).show();
+    }
+}
